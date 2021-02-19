8b4033272f65e157376bd37441ecca99e9321c1e
sunvar-jf <root@Sunilv-mac.local>
2021-02-19 21:04:24 +0530

    modified code to add dependency code

--- multi3/src/main/java/artifactory/test/Multi3.java
+++ multi3/src/main/java/artifactory/test/Multi3.java
@@ -1,11 +1,24 @@
 package artifactory.test;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * Hello world!
  */
 public class Multi3 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         new Multi1();
         System.out.println("Hello World!");
+        try {
+            Class.forName("org.hsqldb.jdbc.JDBCDriver" );
+        } catch (Exception e) {
+            System.err.println("ERROR: failed to load HSQLDB JDBC driver.");
+            e.printStackTrace();
+            return;
+        }
+
+        Connection c = DriverManager.getConnection("jdbc:hsqldb:hsql://localhost/xdb", "SA", "");
     }
 }
